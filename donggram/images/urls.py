a7b2617070c9ab25be8e66462c8577bda2b43f32
dongjun77 <edj0707@naver.com>
2018-08-30 22:45:05 +0900

    variable urls
    get(methods) urls
    variable url data read as get methods

--- donggram/images/urls.py
+++ donggram/images/urls.py
@@ -8,4 +8,16 @@ urlpatterns = [
         view=views.Feed.as_view(), 
         name="feed"
     ),
+    path(
+        "<int:image_id>/like/", # django variable 
+        view=views.LikeImage.as_view(), 
+        name="like_image"
+    ),
 ]
+
+#/images/3/like/
+
+#0 create the url and the view
+#1 take the id from the url
+#2 we want to find an image with this id
+#3 we want to create a like for that image
