18a2179b759e2ee1f664714813f37961442c189b
dongjun77 <edj0707@naver.com>
2018-08-30 18:50:46 +0900

    images models
    1. TimeStapedModel
    2. Creator ForeignKey(user_models.User)
    3. image = models.ForeignKey(Image)
     - Comment or Like

--- donggram/images/models.py
+++ donggram/images/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from donggram.users import models as user_models
 
 # Create your models here.
+@python_2_unicode_compatible
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+@python_2_unicode_compatible
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+
+@python_2_unicode_compatible
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+@python_2_unicode_compatible
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
